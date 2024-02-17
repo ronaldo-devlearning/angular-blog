0e23f3b5ee0b29a47f40276b13e86eeae680c8ac
Ronaldo Rocha <ronaldo.devlearning@gmail.com>
2024-02-17 12:37:31 -0300

    Projeto finalizado

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,25 @@
+export const dataFake = [
+
+    {
+        "id": "1",
+        "photo": "ABC",
+        "title": "Atualização do Angular 2022",
+        "description": "Olá mundo",
+        "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXIUJjOGSnVxR6tKY7wOaW7By15PNPRLWCPCRhNec_7r1ZRBHNAHZEoaJPqAwViA-JFEg&usqp=CAU"
+    },
+    {
+        "id": "2",
+        "photo": "ABC",
+        "title": "Atualização do Angular 2023",
+        "description": "Olá mundão",
+        "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXIUJjOGSnVxR6tKY7wOaW7By15PNPRLWCPCRhNec_7r1ZRBHNAHZEoaJPqAwViA-JFEg&usqp=CAU"
+    },
+    {
+        "id": "3",
+        "photo": "ABC",
+        "title": "Atualização do Angular 2024",
+        "description": "Hello world",
+        "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXIUJjOGSnVxR6tKY7wOaW7By15PNPRLWCPCRhNec_7r1ZRBHNAHZEoaJPqAwViA-JFEg&usqp=CAU"
+    }
+
+]
